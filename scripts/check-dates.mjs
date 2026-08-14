96a24e992d6faa673640a574fba20bb3b6436407
WAnubhavGoel <goelanubhav70@gmail.com>
2026-08-14 08:40:01 +0000

    Revert "Refactor analytics to single-source-of-truth day-by-day loop and fix holiday sentinel logging"
    This reverts commit 8fdad7fb0811a352e23d8d224f469f2f1e0d9d3a.

--- scripts/check-dates.mjs
+++
@@ -1,6 +0,0 @@
-const dates = ['2026-08-11','2026-08-12','2026-08-13','2026-08-14','2026-08-15','2026-08-16','2026-08-18'];
-const names = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
-dates.forEach(ds => {
-  const d = new Date(ds + 'T00:00:00Z');
-  console.log(ds, '=', names[d.getUTCDay()], '(getUTCDay=' + d.getUTCDay() + ')');
-});
